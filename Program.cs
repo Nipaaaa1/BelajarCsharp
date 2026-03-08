635938ed90b349a4374571ebc5abc013571da1bb
Nipaaaa1 <frostone224@gmail.com>
2026-03-08 17:51:38 +0000

    learn: variables

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, dari C#");
+﻿// Variabels
+
+String name = "Nipa";
+int age = 20;
+bool programmer = true;
+char someChar = '#';
+
+Console.WriteLine("Halo, " + name);
+Console.WriteLine("Usia kamu " + age + " tahun");
+Console.WriteLine("Apakah kamu itu programmer? " + programmer);
+Console.WriteLine(someChar);
