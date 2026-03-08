6a8ee5872cde1386d36727acdf1a94317e5965af
Nipaaaa1 <frostone224@gmail.com>
2026-03-08 18:23:25 +0000

    learn: maths

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
-﻿// User Input
+﻿// Maths
 
-Console.WriteLine("Sipa namamu?");
-String name = Console.ReadLine();
-Console.WriteLine("Berapa umurmu?");
-int age = Convert.ToInt16(Console.ReadLine());
+int x = 4;
+int y = 6;
 
-Console.WriteLine("Nama kamu " + name);
-Console.WriteLine("Umur kamu " + age + " tahun");
+double a = 3.14;
+double b = 3.81;
+
+Console.WriteLine(Math.Pow(x,y));
+Console.WriteLine(Math.Sqrt(x));
+Console.WriteLine(Math.Max(x,y));
+Console.WriteLine(Math.Min(x,y));
+Console.WriteLine(Math.Abs(y));
+Console.WriteLine(Math.Round(a));
+Console.WriteLine(Math.Ceiling(a));
+Console.WriteLine(Math.Floor(b));
