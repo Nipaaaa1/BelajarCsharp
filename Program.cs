d668de235ae2d9cf88bfae0217eeac64b07651db
Nipaaaa1 <frostone224@gmail.com>
2026-03-08 18:31:17 +0000

    learn: hypotenuse program

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿// Random
+﻿// Hypotenuse
 
-Random random = new Random();
+Console.WriteLine("Masukkan sisi a:");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Masukkan sisi b:");
+int b = Convert.ToInt16(Console.ReadLine());
 
-Console.WriteLine(random.Next(1,7));
-Console.WriteLine(random.Next(1,7));
-Console.WriteLine(random.Next(1,7));
-Console.WriteLine(random.NextDouble());
+double result = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+Console.WriteLine("Hasilnya: " + result);
