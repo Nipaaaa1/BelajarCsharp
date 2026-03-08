65db162e9ce2d7108f0bdc23ffd8dcf788234a50
Nipaaaa1 <frostone224@gmail.com>
2026-03-08 18:45:46 +0000

    learn: string methods

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-﻿// Hypotenuse
+﻿// String methods
 
-Console.WriteLine("Masukkan sisi a:");
-int a = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine("Masukkan sisi b:");
-int b = Convert.ToInt16(Console.ReadLine());
+String fullName = "Nipa Guys";
+String phoneNumber = "0811-1234-5678";
 
-double result = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
-
-Console.WriteLine("Hasilnya: " + result);
+Console.WriteLine(fullName.ToUpper());
+Console.WriteLine(fullName.ToLower());
+Console.WriteLine(phoneNumber.Replace('-', '#'));
+Console.WriteLine(fullName.Length);
+Console.WriteLine(fullName.Substring(0, 4));
+Console.WriteLine(fullName.Substring(5));
