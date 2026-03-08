40f0d62354ac092264d8be047f0f360b6c398e4b
Nipaaaa1 <frostone224@gmail.com>
2026-03-08 18:26:08 +0000

    learn: random

--- Program.cs
+++ Program.cs
@@ -1,16 +1,8 @@
-﻿// Maths
+﻿// Random
 
-int x = 4;
-int y = 6;
+Random random = new Random();
 
-double a = 3.14;
-double b = 3.81;
-
-Console.WriteLine(Math.Pow(x,y));
-Console.WriteLine(Math.Sqrt(x));
-Console.WriteLine(Math.Max(x,y));
-Console.WriteLine(Math.Min(x,y));
-Console.WriteLine(Math.Abs(y));
-Console.WriteLine(Math.Round(a));
-Console.WriteLine(Math.Ceiling(a));
-Console.WriteLine(Math.Floor(b));
+Console.WriteLine(random.Next(1,7));
+Console.WriteLine(random.Next(1,7));
+Console.WriteLine(random.Next(1,7));
+Console.WriteLine(random.NextDouble());
