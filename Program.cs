b36837970c534f3cf98c25b40cf1fc3be9a7c0d3
Nipaaaa1 <frostone224@gmail.com>
2026-03-08 19:04:36 +0000

    learn: number guessing game

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
-﻿// String methods
+﻿// Number Guessing Game
 
-String fullName = "Nipa Guys";
-String phoneNumber = "0811-1234-5678";
+Console.WriteLine("Welcome to Number Guessing Game");
 
-Console.WriteLine(fullName.ToUpper());
-Console.WriteLine(fullName.ToLower());
-Console.WriteLine(phoneNumber.Replace('-', '#'));
-Console.WriteLine(fullName.Length);
-Console.WriteLine(fullName.Substring(0, 4));
-Console.WriteLine(fullName.Substring(5));
+Random random = new Random();
+
+int hiddenNumber = random.Next(1, 101);
+int guess = 0;
+
+while(hiddenNumber != guess) {
+  Console.WriteLine("Pick a number between 1 - 100:");
+  guess = Convert.ToInt16(Console.ReadLine());
+  
+  if(guess > hiddenNumber) {
+    Console.WriteLine("Too high!");
+  } else if(guess < hiddenNumber) {
+    Console.WriteLine("Too low!");
+  }
+}
+
+Console.WriteLine("You win! The number is: " + guess);
