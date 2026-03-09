984b81f4bd74609c0b32afacc9da125ed181a946
Nipaaaa1 <frostone224@gmail.com>
2026-03-09 17:02:02 +0000

    learn: interface

--- Program.cs
+++ Program.cs
@@ -1,26 +1,40 @@
-﻿// Polymorphism
+﻿// Interface
 
-Cat cat = new Cat();
-Dog dog = new Dog();
+Rabbit rabbit = new Rabbit();
+Wolf wolf = new Wolf();
+Fish fish = new Fish();
 
-Animal[] animals = { cat, dog };
+rabbit.flee();
+wolf.hunt();
+fish.flee();
+fish.hunt();
 
-foreach(Animal animal in animals) {
-  animal.Sound();
+interface IPrey {  
+  public void flee();
 }
 
-class Animal {
-  public virtual void Sound(){}
+interface IPredator {
+  public void hunt();
 }
 
-class Dog : Animal {
-  public override void Sound() {
-    Console.WriteLine("The dog goes woff");
+class Rabbit : IPrey {
+  public void flee() {
+    Console.WriteLine("The Rabbit is fleeing");
   }
 }
 
-class Cat : Animal {
-  public override void Sound() {
-    Console.WriteLine("The cat goes meow");
+class Wolf : IPredator {
+  public void hunt() {
+    Console.WriteLine("The Wolf is hunting");
+    }
+}
+
+class Fish : IPrey, IPredator {
+  public void flee() {
+    Console.WriteLine("The fish swims away");
+  }
+  
+  public void hunt() {
+    Console.WriteLine("The fish is looking for a smaller fish");
   }
 }
