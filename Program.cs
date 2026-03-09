e55bd0236cddf56b484ef06214ce6953be122a8e
Nipaaaa1 <frostone224@gmail.com>
2026-03-09 17:50:40 +0000

    learn: thread

--- Program.cs
+++ Program.cs
@@ -1,14 +1,30 @@
-﻿// Generic
+﻿// Threads
 
-int[] numbers1 = { 1, 2, 3 };
-double[] numbers2 = { 1.0, 2.0, 3.0 };
+Thread mainThread = Thread.CurrentThread;
+mainThread.Name = "Main Thread";
 
-PrintArray(numbers1);
-PrintArray(numbers2);
+Thread thread1 = new Thread(() => CountDown("Timer 1"));
+Thread thread2 = new Thread(() => CountUp("Timer 2"));
 
-void PrintArray<T>(T[] arrays) {
-  foreach(T item in arrays) {
-    Console.Write(item + " ");
+thread1.Start();
+thread2.Start();
+
+Console.WriteLine($"{mainThread.Name} is completed");
+
+
+
+void CountDown(String name) {
+  for(int i = 10; i > 0; i--) {
+    Console.WriteLine($"{name}: {i} seconds" );
+    Thread.Sleep(1000);
   }
-  Console.WriteLine();
+  Console.WriteLine($"{name} is completed");
+}
+
+void CountUp(String name) {
+  for(int i = 0; i < 10; i++) {
+    Console.WriteLine($"{name}: {i} seconds" );
+    Thread.Sleep(1000);
+  }
+  Console.WriteLine($"{name} is completed");
 }
